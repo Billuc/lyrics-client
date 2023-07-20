d26ad9b82e24a220607c34ff8c29da07426b9421
Billuc <billuc@hotmail.fr>
2023-07-20 22:08:24 +0200

    Super.init missing + warnings

--- tests/test_azlyrics_client.py
+++ tests/test_azlyrics_client.py
@@ -7,7 +7,7 @@ from lyrics_client.classes.fetch_lyrics_command import FetchLyricsCommand
 
 my_vcr = generate_vcr("test_azlyrics_client")
 
-@my_vcr.use_cassette()
+@my_vcr.use_cassette
 def test_headers_session_and_xcode_initialized():
     client = AZLyricsLyricsClient()
 
@@ -16,14 +16,14 @@ def test_headers_session_and_xcode_initialized():
     assert client.x_code is not None
 
 
-@my_vcr.use_cassette()
+@my_vcr.use_cassette
 def test_client_name_is_azlyrics():
     client = AZLyricsLyricsClient()
 
     assert client.client_name == "azlyrics"
 
 
-@my_vcr.use_cassette()
+@my_vcr.use_cassette
 def test_search_songs():
     client = AZLyricsLyricsClient()
     command = FetchLyricsCommand("in the end", "linkin park", ["azlyrics"])
@@ -34,7 +34,7 @@ def test_search_songs():
     assert '"In The End" - Linkin Park' in search_results
 
 
-@my_vcr.use_cassette()
+@my_vcr.use_cassette
 def test_fetch_lyrics():
     URL = 'https://www.azlyrics.com/lyrics/linkinpark/intheend.html'
     EXPECTED_LYRICS = "[Verse 1: Mike Shinoda, Chester Bennington, Both]\nIt starts with one\nOne thing, I don't know why\nIt doesn't even matter how hard you try\nKeep that in mind, I designed this rhyme\nTo explain in due time, all I know\nTime is a valuable thing\nWatch it fly by as the pendulum swings\nWatch it count down to the end of the day\nThe clock ticks life away, it's so unreal\nDidn't look out below\nWatch the time go right out the window\nTryin' to hold on, d-didn't even know\nI wasted it all just to watch you go\n\n[Pre-Chorus: Mike Shinoda]\nI kept everything inside\nAnd even though I tried, it all fell apart\nWhat it meant to me will eventually be\nA memory of a time when I tried so hard\n\n[Chorus: Chester Bennington]\nI tried so hard and got so far\nBut in the end, it doesn't even matter\nI had to fall to lose it all\nBut in the end, it doesn't even matter\n\n[Verse 2: Mike Shinoda, Both]\nOne thing, I don't know why\nIt doesn't even matter how hard you try\nKeep that in mind, I designed this rhyme\nTo remind myself how I tried so hard\nIn spite of the way you were mockin' me\nActin' like I was part of your property\nRemembering all the times you fought with me\nI'm surprised it got so far\nThings aren't the way they were before\nYou wouldn't even recognize me anymore\nNot that you knew me back then\nBut it all comes back to me in the end\n\n[Pre-Chorus: Mike Shinoda]\nYou kept everything inside\nAnd even though I tried, it all fell apart\nWhat it meant to me will eventually be\nA memory of a time when I tried so hard\n\n[Chorus: Chester Bennington]\nI tried so hard and got so far\nBut in the end, it doesn't even matter\nI had to fall to lose it all\nBut in the end, it doesn't even matter\n\n[Bridge: Chester Bennington]\nI've put my trust in you\nPushed as far as I can go\nFor all this, there's only one thing you should know\nI've put my trust in you\nPushed as far as I can go\nFor all this, there's only one thing you should know\n\n[Chorus: Chester Bennington]\nI tried so hard and got so far\nBut in the end, it doesn't even matter\nI had to fall to lose it all\nBut in the end, it doesn't even matter"
@@ -46,7 +46,7 @@ def test_fetch_lyrics():
     assert fuzz.ratio(EXPECTED_LYRICS, lyrics) > 80
 
 
-@my_vcr.use_cassette()
+@my_vcr.use_cassette
 def test_get_lyrics():
     EXPECTED_LYRICS = "[Verse 1: Mike Shinoda, Chester Bennington, Both]\nIt starts with one\nOne thing, I don't know why\nIt doesn't even matter how hard you try\nKeep that in mind, I designed this rhyme\nTo explain in due time, all I know\nTime is a valuable thing\nWatch it fly by as the pendulum swings\nWatch it count down to the end of the day\nThe clock ticks life away, it's so unreal\nDidn't look out below\nWatch the time go right out the window\nTryin' to hold on, d-didn't even know\nI wasted it all just to watch you go\n\n[Pre-Chorus: Mike Shinoda]\nI kept everything inside\nAnd even though I tried, it all fell apart\nWhat it meant to me will eventually be\nA memory of a time when I tried so hard\n\n[Chorus: Chester Bennington]\nI tried so hard and got so far\nBut in the end, it doesn't even matter\nI had to fall to lose it all\nBut in the end, it doesn't even matter\n\n[Verse 2: Mike Shinoda, Both]\nOne thing, I don't know why\nIt doesn't even matter how hard you try\nKeep that in mind, I designed this rhyme\nTo remind myself how I tried so hard\nIn spite of the way you were mockin' me\nActin' like I was part of your property\nRemembering all the times you fought with me\nI'm surprised it got so far\nThings aren't the way they were before\nYou wouldn't even recognize me anymore\nNot that you knew me back then\nBut it all comes back to me in the end\n\n[Pre-Chorus: Mike Shinoda]\nYou kept everything inside\nAnd even though I tried, it all fell apart\nWhat it meant to me will eventually be\nA memory of a time when I tried so hard\n\n[Chorus: Chester Bennington]\nI tried so hard and got so far\nBut in the end, it doesn't even matter\nI had to fall to lose it all\nBut in the end, it doesn't even matter\n\n[Bridge: Chester Bennington]\nI've put my trust in you\nPushed as far as I can go\nFor all this, there's only one thing you should know\nI've put my trust in you\nPushed as far as I can go\nFor all this, there's only one thing you should know\n\n[Chorus: Chester Bennington]\nI tried so hard and got so far\nBut in the end, it doesn't even matter\nI had to fall to lose it all\nBut in the end, it doesn't even matter"
 
@@ -59,5 +59,6 @@ def test_get_lyrics():
     assert result.client == "azlyrics"
     assert result.song_title == "in the end"
     assert result.song_artists == "linkin park"
+    assert result.lyrics is not None
     assert fuzz.ratio(EXPECTED_LYRICS, result.lyrics) > 80
     assert result.exception is None
